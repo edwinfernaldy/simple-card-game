89a38e012af934eceba47724678e7081f34cfc27
edwinfernaldy <edwinfernaldy@gmail.com>
2023-06-11 00:46:26 +0700

    add random

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,25 +1,47 @@
+"use client";
+
+import { useState } from "react";
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
 export default function Board() {
-  const matrix = [
-    [1, 1, 1],
-    [2, 2, 2],
-    [3, 3, 3]
-  ];
+  const [mat, setMat] = useState([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ]);
+
+  const handleClick = (r: number, c: number) => {
+    let random = getRandomInt(13);
+
+    if (random == 0) {
+      random++;
+    }
+
+    let tmp = [...mat];
+    tmp[r][c] = random;
+
+    setMat(tmp);
+  };
 
   return (
     <div className='grid grid-flow-row gap-4'>
-      {matrix.map((data, i) => {
+      {mat.map((data, i) => {
         return (
           <div key={i} className='grid grid-flow-col gap-4'>
-            {data.map((data, i) => {
-              return (
-                <div
-                  className='border flex cursor-pointer flex-col justify-center text-center border-gray-700 rounded-lg p-10'
-                  key={i}
-                >
-                  {data}
-                </div>
-              );
-            })}
+            {data.map((data, j) => (
+              <button
+                className='border bg-red-200 text-black hover:scale-105 transition-all flex cursor-pointer justify-center text-center border-gray-700 rounded-lg p-10'
+                key={j}
+                onClick={() => {
+                  handleClick(i, j);
+                }}
+              >
+                {data}
+              </button>
+            ))}
           </div>
         );
       })}
