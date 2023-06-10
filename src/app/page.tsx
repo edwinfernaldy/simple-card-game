753a609ded7647fd459ff4d2668c30b006995a3f
edwinfernaldy <edwinfernaldy@gmail.com>
2023-06-11 00:03:28 +0700

    change to flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Title from "../../components/Title";
 
 export default function Home() {
   return (
-    <main className='min-h-screen p-24 grid grid-rows-2'>
+    <main className='min-h-screen p-24 flex flex-col justify-evenly'>
       <div className='text-center'>
         <Title>Simple Card Game</Title>
         <p>Guess the next card.</p>
