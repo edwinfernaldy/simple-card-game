d733aac4f16a524f65828db3e3f35650b379f17a
edwinfernaldy <edwinfernaldy@gmail.com>
2023-06-11 09:52:05 +0700

    add hover

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -32,15 +32,28 @@ export default function Board() {
         return (
           <div key={i} className='grid grid-flow-col md:gap-4 gap-2'>
             {data.map((data, j) => (
-              <button
-                className='border bg-red-200 text-black hover:scale-105 transition-all flex cursor-pointer justify-center text-center border-gray-700 rounded-lg p-10'
-                key={j}
-                onClick={() => {
-                  handleClick(i, j);
-                }}
-              >
-                <h1 className='font-black text-lg md:text-3xl'>{data}</h1>
-              </button>
+              <div className='relative group' key={j}>
+                <button
+                  className='border z-0 bg-red-200 w-full text-black hover:scale-105 transition-all flex cursor-pointer justify-center text-center border-gray-700 rounded-lg p-10'
+                  onClick={() => {
+                    handleClick(i, j);
+                  }}
+                >
+                  <h1 className='font-black text-lg md:text-3xl'>{data}</h1>
+                </button>
+
+                <div className='absolute inset-0 md:inset-10 z-50 invisible group-hover:visible'>
+                  <div className='flex h-full flex-col md:flex-row justify-between'>
+                    <h1 className='p-3 self-center rounded-2xl backdrop-blur-md bg-white/30 hover:scale-105 transition-all cursor-pointer'>
+                      High
+                    </h1>
+
+                    <h1 className='p-3 self-center rounded-2xl backdrop-blur-md bg-white/30 hover:scale-105 transition-all cursor-pointer'>
+                      Low
+                    </h1>
+                  </div>
+                </div>
+              </div>
             ))}
           </div>
         );
